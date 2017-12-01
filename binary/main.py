464b890d28c51b01d6460320dd208f3984db6666
Spencer Herzberg <spencer.herzberg@gmail.com>
2017-11-30 22:58:29 -0600

    latest

--- binary/main.py
+++ binary/main.py
@@ -2,6 +2,10 @@ from library_one import library_one
 from library_two import library_two
 
 
-if __name__ == '__main__':
+def main():
     library_one.one()
     library_two.two()
+
+
+if __name__ == '__main__':
+    main()
